77e785d6bf8c0b87840821c7d77fcfd098a6e017
Cesar Chavarriaga <tronos_123@hotmail.com>
2025-06-02 18:56:16 -0500

    elegir idioma 12

--- app.py
+++ app.py
@@ -110,7 +110,8 @@ def get_user_language(user_id):
     #obtiene el idioma preferido
     users = load_user_preferences_from_sheet()
     #users = user_id
-    return users.get(user_id, {}).get("language","en")#por defecto ingles
+    #return users.get(user_id, {}).get("language","en")#por defecto ingles
+    return "e"
 
 
 def load_users():
